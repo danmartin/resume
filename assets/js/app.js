50ce999c3b33a58ea6378f82b365b3771f4d5e2a
Dan Martin <dmartin@geiger.com>
2017-09-20 10:55:48 -0700

    Initial resume commit

---
+++ assets/js/app.js
@@ -0,0 +1,28 @@
+"use strict";
+var app = angular.module("resume", []);
+
+app.factory("fetchResume", ['$http', '$q',  function($http, $q){
+    var deferred = $q.defer();
+    return {
+        getJson: function(){
+            $http.get("./assets/data/resume.json")
+                .then(function(response){
+                    deferred.resolve(response.data);
+                },
+                function(response){
+                    console.log(response);
+                });
+                return deferred.promise;
+        }
+    }
+
+}]);
+
+app.controller('MainCtrl', ['$scope', 'fetchResume',  function($scope, fetchResume) {
+
+    fetchResume.getJson().then(function(data) {
+        console.log(data);
+        $scope.resume = data;
+    });
+
+}]);
